b991b21cce0c4f389510cb9c822489629be78738
Erick Martinez <analyst_martinez@outlook.com>
2022-07-31 16:59:12 -0400

    To add command line arguments functionality

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Whois
 
-Python whois tool.
+Example:
+
+From the command line:
+
+`python whois.py 204.79.197.200`
+
+Lookup Information:
+------------------------------
+204.79.197.200
+Microsoft Corporation
+Redmond WA 98052
+US
+------------------------------
--- whois.py
+++ whois.py
@@ -1,6 +1,17 @@
 
+import sys
 from ipwhois import IPWhois
 
-obj = IPWhois('20.189.173.1')
-who = obj.lookup_whois()
-print(who)
+def who(ipaddress):
+
+    obj = IPWhois(ipaddress)
+    who = obj.lookup_whois()['nets'][0]
+    
+    data = '\nLookup Information:' + '\n'+'-'*30 + "\n" + ipaddress + "\n" \
+            + who["description"] + f'\n{who["city"]} {who["state"]} {who["postal_code"]}' + "\n" + who["country"]  + '\n'+'-'*30
+    
+    return data
+
+if __name__ == '__main__':
+    print(who(sys.argv[1]))
+
